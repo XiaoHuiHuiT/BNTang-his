9a3b5c5732fb0d2552cda72e960aba0c82fc9643
tangyihao <leadertangbn@gmail.com>
2020-09-01 19:41:53 +0800

    system模块：api中添加作者

--- his-system/system-api/src/main/java/com/it6666/service/UserService.java
+++ his-system/system-api/src/main/java/com/it6666/service/UserService.java
@@ -2,6 +2,10 @@ package com.it6666.service;
 
 import com.it6666.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author BNTang
+ */
 public interface UserService extends IService<User>{
 
 
