19e004de6a11dc27620062fd15077c3206e506f3
tangyihao <leadertangbn@gmail.com>
2020-09-02 19:38:30 +0800

    system模块：删除无用导包

--- his-system/system-service/src/main/java/com/it6666/service/impl/UserServiceImpl.java
+++ his-system/system-service/src/main/java/com/it6666/service/impl/UserServiceImpl.java
@@ -2,7 +2,6 @@ package com.it6666.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.stereotype.Service;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.it6666.mapper.UserMapper;
 import com.it6666.domain.User;
 import com.it6666.service.UserService;
