e591a2e6dfa656b5c6a850eefd05ec79ba0f20b4
BNTang <leadertangbn@gmail.com>
2020-09-10 00:10:04 +0800

    system模块：操作日志的生成

---
+++ his-doctor/doctor-service/src/main/java/com/it6666/service/OperLogService.java
@@ -0,0 +1,14 @@
+package com.it6666.service;
+
+import com.it6666.domain.OperLog;
+import com.baomidou.mybatisplus.extension.service.IService;
+    /**
+ * description: OperLogService
+ * date: 2020-09-09 23:20
+ * author: 30315
+ * version: 1.0
+ */
+public interface OperLogService extends IService<OperLog>{
+
+
+}
---
+++ his-system/system-mapper/src/main/java/com/it6666/mapper/OperLogMapper.java
@@ -0,0 +1,13 @@
+package com.it6666.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.it6666.domain.OperLog;
+
+/**
+ * description: OperLogMapper
+ * date: 2020-09-09 23:20
+ * author: 30315
+ * version: 1.0
+ */
+public interface OperLogMapper extends BaseMapper<OperLog> {
+}
